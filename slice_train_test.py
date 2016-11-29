20f824f718ce524f01d526f6b17ca4871249b8a0
Dan Bikle <bikle@bikle.com>
2016-11-28 18:07:57 -0800

    more

---
+++ slice_train_test.py
@@ -0,0 +1,14 @@
+# slice_train_test.py
+
+# This script should slice iris.csv into /tmp/iris_train.csv and /tmp/iris_test.csv
+
+import pandas as pd
+import pdb
+
+# I should read iris.csv into a DataFrame:
+
+iris0_df = pd.read_csv('iris.csv')
+
+# I should randomize rows
+
+'bye'

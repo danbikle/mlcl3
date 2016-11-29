35cb2e5a8f2b9fa31b83d19a053fbf0ba5e9aedc
Dan Bikle <bikle@bikle.com>
2016-11-28 20:22:25 -0800

    more

--- learn_predict_linr.py
+++ learn_predict_linr.py
@@ -17,14 +17,11 @@ linr_model = linear_model.LinearRegression()
 # I should call fit() to create the model:
 linr_model.fit(x_a, train_df.f1)
 
-# I should save the model:
+test_df = pd.read_csv('/tmp/iris_test.csv')
+test_a = np.array(test_df[['f0','f2','f3','iris_type']])
+predictions_l = linr_model.predict(test_a).tolist()
 
-w0_f = linr_model.intercept_
-w_l  = linr_model.coef_
-m_l  = [w0_f] + w_l.tolist()
-m_sr = pd.Series(m_l)
-m_s  = '/tmp/iris_model.csv'
-m_sr.to_csv(m_s, float_format='%4.2f', index=False, header=['weights'])
-print('Model saved to: '+ '/tmp/iris_model.csv')
+print(predictions_l)
+
 'bye'
 

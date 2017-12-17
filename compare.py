a9d58cf19d36daa79ff1ea62958b815b4b730587
Dan Bikle <danbikle@outlook.com>
2017-12-16 22:37:38 -0800

    more

--- compare.py
+++ compare.py
@@ -7,19 +7,19 @@ import numpy  as np
 import pdb
 
 # I should get the data I want to compare:
-predicitons_df         = pd.read_csv('/tmp/iris_predicitons.csv')
+predictions_df         = pd.read_csv('/tmp/iris_predictions.csv')
 
-predicitons_df['difference'] = predicitons_df.f1 - predicitons_df.prediction
+predictions_df['difference'] = predictions_df.f1 - predictions_df.prediction
 
-predicitons_df['diff_squared'] = predicitons_df.difference ** 2
+predictions_df['diff_squared'] = predictions_df.difference ** 2
 
-print('A comparison of observed values (f1) and predicitons:')
-print(predicitons_df)
+print('A comparison of observed values (f1) and predictions:')
+print(predictions_df)
 
 print('The square-root of the mean of differences-squared:')
-print(np.mean(predicitons_df.diff_squared) ** 0.5)
+print(np.mean(predictions_df.diff_squared) ** 0.5)
 print('Acronym for above calculation is "RMSE".')
-print('For me, RMSE is a good way to compare observed values and predicitons.')
+print('For me, RMSE is a good way to compare observed values and predictions.')
 print('If RMSE is zero, the predictions are probably accurate.')
 
 'bye'
--- learn_predict_linr.py
+++ learn_predict_linr.py
@@ -23,9 +23,9 @@ predictions_l = linr_model.predict(test_a).tolist()
 
 # I should write the predictions to CSV in a way which helps me compare then to observations:
 test_df['prediction'] = predictions_l
-test_df[['f0','f2','f3','iris_type','f1','prediction']].to_csv('/tmp/iris_predicitons.csv', float_format='%4.2f', index=False)
+test_df[['f0','f2','f3','iris_type','f1','prediction']].to_csv('/tmp/iris_predictions.csv', float_format='%4.2f', index=False)
 
-print('Predictions should be here: /tmp/iris_predicitons.csv')
+print('Predictions should be here: /tmp/iris_predictions.csv')
 
 'bye'
 
